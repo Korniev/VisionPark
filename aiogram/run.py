cc658c96a608413bc40829de380dfa278bde9fc7
Danylo <114859769+Almixs@users.noreply.github.com>
2024-05-08 08:27:01 +0300

    append router

--- aiogram/run.py
+++ aiogram/run.py
@@ -1,23 +1,22 @@
-import aiogram
+import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
 from config import TOKEN
+from app.handlers import router
 
 bot = Bot(token=TOKEN)
-db = Dispatcher()
-
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer("Привіт")
-
+dp = Dispatcher()
 
 
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('EXIT')
