81d7fdefee12509426918ceb726266d5708f28e7
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-05-09 13:34:18 +0200

    add new migrations

--- vision_park/recognize/models.py
+++ vision_park/recognize/models.py
@@ -10,8 +10,8 @@ from parking_area.models import ParkingSpace
 
 
 class Car(models.Model):
-    license_plate = models.CharField(max_length=15, unique=True)
-    owner = models.ForeignKey(CustomUser, blank=True, on_delete=models.CASCADE, related_name='cars')
+    license_plate = models.CharField(max_length=16, unique=True)
+    owner = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.CASCADE, related_name='cars')
     # photo_car = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
     # predict = models.FloatField(null=True)
     # PayPass = models.BooleanField(default=False)
