720085e1ad2210bf88cc62469975dc66e5bcfa64
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-05-12 12:50:27 +0200

    upd recognize main and urls

--- vision_park/recognize/urls.py
+++ vision_park/recognize/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # path("", views.main, name="index"),
     path("main", views.main, name="main"),
     path("upload_in", views.upload_in, name="upload_in"),
+    path("upload_out", views.upload_out, name="upload_out"),
     path("result_in", views.create_parking_session, name="result_in"),
     path("session_view", views.session_view, name="session_view"),
     path('session_action/<int:pk>/', views.session_action, name='session_action'),
