92bf4cb892a0734fb88ed7e6dc7457972e47c362
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-05-15 14:59:53 +0200

    upd url for website

--- vision_park/accounts/keyboards.py
+++ vision_park/accounts/keyboards.py
@@ -8,4 +8,4 @@ main = ReplyKeyboardMarkup(keyboard=[
     resize_keyboard=True,
     input_field_placeholder='waiting for a menu item...')
 
-link_website = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Open website', url='http://127.0.0.1:8000/auth/profile/')]])
+link_website = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Open website', url='http://0.0.0.0:8000/')]])
