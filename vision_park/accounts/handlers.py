c5291f34b0cf406693b828bf4817b2cd6beed295
Danylo <114859769+Almixs@users.noreply.github.com>
2024-05-15 14:58:46 +0300

    add keyboards and ended link to website

--- vision_park/accounts/handlers.py
+++ vision_park/accounts/handlers.py
@@ -1,9 +1,11 @@
-from aiogram import Router, types
-# from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram import Router, types, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
 from asgiref.sync import sync_to_async
 
 from accounts.models import CustomUser
+
+import accounts.keyboards as kb
 
 handler_router = Router()
 
@@ -14,6 +16,12 @@ async def start(message: types.Message):
 
     try:
         user = await sync_to_async(CustomUser.objects.get)(username=username)
-        await message.answer(f"Welcome back, {user.username}!")
+        await message.answer(f"Welcome back, {user.username}!",
+                             reply_markup=kb.main)
     except CustomUser.DoesNotExist:
         await message.answer("User not found. Please register first!")
+
+@handler_router.message(F.text == '🌐Website')
+async def get_link_website(message: Message):
+    await message.answer('Here is the link to the website',
+                         reply_markup=kb.link_website)
---
+++ vision_park/accounts/keyboards.py
@@ -0,0 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='🚘My cars')],
+    [KeyboardButton(text='💲Price'), KeyboardButton(text='🌐Website')],
+    [KeyboardButton(text='ℹ️Available parking'), KeyboardButton(text='🙌Support')]
+],
+    resize_keyboard=True,
+    input_field_placeholder='waiting for a menu item...')
+
+link_website = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Open website', url='http://127.0.0.1:8000/auth/profile/')]])
